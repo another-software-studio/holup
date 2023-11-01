0740dcc13c1699ad138eb9eed07f180d652d876f
MichalLopaciuch <michal.lopaciuch@gmail.com>
2023-11-01 22:31:01 +0100

    Add request handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,68 @@
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
+#include <boost/config.hpp>
 
 #include <spdlog/spdlog.h>
-#include <sys/socket.h>
 
-#include <iostream>
 #include <thread>
 #include <string>
 #include <memory>
 
 #define ARG_ERROR 1
 #define UNX_ERROR 2
-
-#define MIN_ARGC 2
 #define DEF_PORT 9999
 #define DEF_HOST "0.0.0.0"
 
 namespace beast = boost::beast;
 namespace http  = beast::http;
 namespace net   = boost::asio;
-using tcp       = boost::asio::ip::tcp;
+    using tcp   = boost::asio::ip::tcp;
+
+template<typename Body, typename Allocator>
+http::message_generator handle_request(http::request<Body, http::basic_fields<Allocator>>&& req) {
+    spdlog::info("handling request");
+    http::response<http::empty_body> res{http::status::ok, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "application/text");
+    res.keep_alive(req.keep_alive());
+    spdlog::info("sending response back");
+
+    return res;
+}
 
 void do_session(tcp::socket& socket) {
-    beast::error_code ec;
-
     spdlog::info("request received");
+    beast::error_code ec;
+    beast::flat_buffer buffer;
 
+    while(true) {
+        http::request<http::string_body> req;
+        spdlog::info("reading request");
+        http::read(socket, buffer, req, ec);
+
+        if (http::error::end_of_stream == ec) {
+            break;
+        }
+        if (ec) {
+            return spdlog::error("{0} read", ec.message());
+        }
+
+        spdlog::info("after stream check");
+        http::message_generator msg =
+            handle_request(std::move(req));
+
+        bool keep_alive = msg.keep_alive();
+        beast::write(socket, std::move(msg), ec);
+
+        if (ec) {
+            return spdlog::error("{0} write", ec.message());
+        }
+        if (!keep_alive) {
+            break;
+        }
+    }
     socket.shutdown(tcp::socket::shutdown_send, ec);
 }
 
@@ -45,8 +82,10 @@ int main(int argc, char* argv[]) noexcept {
         
         net::io_context ioc{1};
         tcp::acceptor acceptor{ioc, {address, port}};
+        spdlog::info("initialization done");
 
         while(true) {
+            spdlog::info("listening...");
             tcp::socket socket{ioc};
             acceptor.accept(socket);
 
